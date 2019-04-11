2e0283a83c366c5bd492e5d8848d4778999ec024
Nelly Khmeleva <nkhmeleva@yahoo.com>
2019-04-11 16:58:05 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,10 @@
+function iterativeLog(array){
+  array.forEach((element,index) => {
+    console.log(`${index}: ${element}`);
+  })
+}
+function iterate(callback){
+  array.forEach((element,index)=>{
+    return array;
+  });
+}
